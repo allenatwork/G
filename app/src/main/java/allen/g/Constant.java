0e52ac27df7c8cbb0e1fbae2d58115ac50b29438
anhdq4 <anhdq4@vng.com.vn>
2017-11-28 18:59:54 +0700

    login success with google

---
+++ app/src/main/java/allen/g/Constant.java
@@ -0,0 +1,10 @@
+package allen.g;
+
+/**
+ * Created by local on 28/11/2017.
+ */
+
+public class Constant {
+    public static final String BASE_URL = "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable";
+
+}
--- app/src/main/java/allen/g/MainActivity.java
+++ app/src/main/java/allen/g/MainActivity.java
@@ -1,62 +1,174 @@
+/**
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ * <p>
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ * <p>
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package allen.g;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.drive.CreateFileActivityOptions;
 import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveClient;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveResourceClient;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 
-public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "TEST";
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "ZBackupDrive";
+    private static final int REQUEST_CODE_SIGN_IN = 0;
+
     private GoogleSignInClient mGoogleSignInClient;
+    private DriveResourceClient mDriveResourceClient;
+
+    private TextView tv;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mGoogleSignInClient = buildGoogleSignInClient();
+        setContentView(R.layout.activity_test);
+        tv = findViewById(R.id.tv);
+        signIn();
     }
 
+    private void signIn() {
+        Log.i(TAG, "Start sign in");
+        mGoogleSignInClient = buildGoogleSignInClient();
+        startActivityForResult(mGoogleSignInClient.getSignInIntent(), REQUEST_CODE_SIGN_IN);
+    }
+
+    /**
+     * Build a Google SignIn client.
+     */
     private GoogleSignInClient buildGoogleSignInClient() {
         GoogleSignInOptions signInOptions =
                 new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                        .requestScopes(Drive.SCOPE_FILE)
+                        .requestScopes(Drive.SCOPE_APPFOLDER)
+                        .requestIdToken(getString(R.string.default_web_client_id))
                         .build();
         return GoogleSignIn.getClient(this, signInOptions);
     }
 
-//    private void updateViewWithGoogleSignInAccountTask(Task<GoogleSignInAccount> task) {
-//        Log.i(TAG, "Update view with sign in account task");
-//        task.addOnSuccessListener(
-//                new OnSuccessListener<GoogleSignInAccount>() {
-//                    @Override
-//                    public void onSuccess(GoogleSignInAccount googleSignInAccount) {
-//                        Log.i(TAG, "Sign in success");
-//                        // Build a drive client.
-//                        mDriveClient = Drive.getDriveClient(getApplicationContext(), googleSignInAccount);
-//                        // Build a drive resource client.
-//                        mDriveResourceClient =
-//                                Drive.getDriveResourceClient(getApplicationContext(), googleSignInAccount);
-//                        // Start camera.
-//                        startActivityForResult(
-//                                new Intent(MediaStore.ACTION_IMAGE_CAPTURE), REQUEST_CODE_CAPTURE_IMAGE);
-//                    }
-//                })
-//                .addOnFailureListener(
-//                        new OnFailureListener() {
-//                            @Override
-//                            public void onFailure(@NonNull Exception e) {
-//                                Log.w(TAG, "Sign in failed", e);
-//                            }
-//                        });
-//    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE_SIGN_IN:
+                Log.i(TAG, "Sign in request code");
+                if (resultCode == RESULT_OK) {
+                    Log.i(TAG, "Signed in successfully.");
+                    GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+                    mDriveResourceClient = Drive.getDriveResourceClient(this, account);
+                    handleDrive(account);
+                    updateUI(account);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Signin not success", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account != null) {
+            tv.setText(account.getDisplayName()+" - " + account.getIdToken());
+        } else {
+            tv.setText("SignIn not success");
+        }
+    }
+
+    private void handleDrive(GoogleSignInAccount gacc) {
+//        createFileInAppFolder();
+    }
+
+    private void createFileInAppFolder() {
+        final Task<DriveFolder> appFolderTask = getDriveResourceClient().getAppFolder();
+        final Task<DriveContents> createContentsTask = getDriveResourceClient().createContents();
+        Tasks.whenAll(appFolderTask, createContentsTask)
+                .continueWithTask(new Continuation<Void, Task<DriveFile>>() {
+                    @Override
+                    public Task<DriveFile> then(@NonNull Task<Void> task) throws Exception {
+                        DriveFolder parent = appFolderTask.getResult();
+                        DriveContents contents = createContentsTask.getResult();
+                        OutputStream outputStream = contents.getOutputStream();
+                        try {
+                            Writer writer = new OutputStreamWriter(outputStream);
+                            writer.write("Hello World!");
+                        } catch (IOException e) {
+                            // Ignore
+                        }
+
+                        MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                                .setTitle("New file")
+                                .setMimeType("text/plain")
+                                .setStarred(true)
+                                .build();
+
+                        return getDriveResourceClient().createFile(parent, changeSet, contents);
+                    }
+                })
+                .addOnSuccessListener(this,
+                        new OnSuccessListener<DriveFile>() {
+                            @Override
+                            public void onSuccess(DriveFile driveFile) {
+                                showMessage(getString(R.string.file_created) +
+                                        driveFile.getDriveId().encodeToString());
+//                                finish();
+                            }
+                        })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Unable to create file", e);
+                        showMessage(getString(R.string.file_create_error));
+//                        finish();
+                    }
+                });
+    }
+
+    private void showMessage(String string) {
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+    }
+
+    private DriveResourceClient getDriveResourceClient() {
+        return mDriveResourceClient;
+    }
+
+
+
 }
